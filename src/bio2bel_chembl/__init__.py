8dac07ee9bc6dcbd58be7a5814d3640dc9de4fae
aramgrigoryan <aramgrigoryan@protonmail.ch>
2017-07-26 13:28:53 +0200

    add setup.py, closes #11

---
+++ src/bio2bel_chembl/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_chembl'
+__description__ = "A package for converting ChEMBL to BEL"
+__url__ = 'https://github.com/bio2bel/chembl'
+
+__author__ = 'Aram Grigoryan'
+__email__ = 'aram.grigoryan@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Aram Grigoryan'
--- src/bio2bel_chembl/cli.py
+++ src/bio2bel_chembl/cli.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
 
-#from .chemical_namespace import write_chemical_belns
-#import click
-#TODO
+import sys
 
+import click
+from pybel_tools.resources import get_today_arty_namespace, deploy_namespace
+
+from .chemical_namespace import write_chemical_belns
+
+
+@click.group(help='cli for bio2bel chembl')
+def main():
+    pass
+
+
+@main.command()
+@click.option('--output', type=click.File('w'), default=sys.stdout)
+def write(output):
+    write_chemical_belns(output)
+
+
+@main.command()
+def deploy():
+    """deploys chemical namespace to artifactory"""
+    f_name = get_today_arty_namespace("chembla")
+
+    with open(f_name, 'w') as file:
+        write_chemical_belns(file)
+
+    deploy_namespace(f_name, 'chembla')
+
+
+if __name__ == '__main__':
+    main()
