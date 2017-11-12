c7098b3037d93c513a62f63b4a13fa33e5f5b8d1
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-12 10:55:12 +0100

    Reformat code

--- src/bio2bel_chembl/chembl_to_inchi.py
+++ src/bio2bel_chembl/chembl_to_inchi.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 """Download ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_23_chemreps.txt.gz
 Unpack chembl_23_chemreps.txt into CHEMBL_DATA_DIR"""
-import os
-import sqlite3
 import logging
+import os
+
 import pandas as pd
 
 log = logging.getLogger(__name__)
 
 from pybel.constants import PYBEL_DATA_DIR
-from pybel_tools.definition_utils import write_namespace
 from pybel_tools.document_utils import write_boilerplate
-from pybel.constants import IS_A
 from pybel.utils import ensure_quotes
-from pybel_tools.resources import INCHI_PATTERN
+from pybel.resources.defaults import INCHI_PATTERN
 
 CHEMBL_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'bio2bel', 'chembl')
 if not os.path.exists(CHEMBL_DATA_DIR):
     os.makedirs(CHEMBL_DATA_DIR)
 
 CHEMBL_CHEMREPS_FILE = os.path.join(CHEMBL_DATA_DIR, 'chembl_23_chemreps.txt')
+
 
 def get_data():
     """Gets the source data.
@@ -33,9 +32,10 @@ def get_data():
         CHEMBL_CHEMREPS_FILE,
         sep='\t',
         skiprows=1,
-        names=['chembl_id',	'canonical_smiles',	'standard_inchi',	'standard_inchi_key']
+        names=['chembl_id', 'canonical_smiles', 'standard_inchi', 'standard_inchi_key']
     )
     return df
+
 
 def write_chembl_inchi_belscript(file, df=None):
     """Writes the ChEMBL ID to inchi equivalence a BEL script
@@ -47,7 +47,7 @@ def write_chembl_inchi_belscript(file, df=None):
     df = get_data() if df is None else df
 
     write_boilerplate(
-        document_name='ChEMBL ID to inchi equivalence',
+        name='ChEMBL ID to inchi equivalence',
         authors='Aram Grigoryan',
         contact='aram.grigoryan@scai.fraunhofer.de',
         licenses='Creative Commons by 4.0',
@@ -66,7 +66,7 @@ def write_chembl_inchi_belscript(file, df=None):
     print('SET Citation = {{"URL","{}"}}'.format("http://chembl.blogspot.de/2017/05/chembl23-released.html"), file=file)
     print('SET Evidence = "ChEMBL to INCHI mapping"', file=file)
 
-    for _,chembl_id, standard_inchi in df[['chembl_id', 'standard_inchi']].itertuples():
+    for _, chembl_id, standard_inchi in df[['chembl_id', 'standard_inchi']].itertuples():
         chembl_clean = ensure_quotes(str(chembl_id).strip())
         inchi_clean = ensure_quotes(str(standard_inchi).strip())
 
@@ -75,10 +75,11 @@ def write_chembl_inchi_belscript(file, df=None):
 
         print('a(CHEMBLA:{}) eq a(INCHI:{})'.format(chembl_clean, inchi_clean), file=file)
 
+
 if __name__ == '__main__':
     log.setLevel(20)
     logging.basicConfig(level=20)
     dff = get_data()
     with open(os.path.join(CHEMBL_DATA_DIR, 'chembl_to_inchi.bel'), 'w') as f:
         write_chembl_inchi_belscript(f, dff)
-    #log.info(dff.head())
+        # log.info(dff.head())
