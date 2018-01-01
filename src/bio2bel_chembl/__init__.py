9023d0bee2ea07af8bdc37c5f938db143b53ebda
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-01-01 18:27:58 +0100

    Update license

--- src/bio2bel_chembl/__init__.py
+++ src/bio2bel_chembl/__init__.py
@@ -8,8 +8,8 @@ __title__ = 'bio2bel_chembl'
 __description__ = "A package for converting ChEMBL to BEL"
 __url__ = 'https://github.com/bio2bel/chembl'
 
-__author__ = 'Aram Grigoryan'
-__email__ = 'aram.grigoryan@scai.fraunhofer.de'
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
 
-__license__ = 'Apache 2.0 License'
-__copyright__ = 'Copyright (c) 2017 Aram Grigoryan'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright (c) 2017-2018 Charles Tapley Hoyt'
