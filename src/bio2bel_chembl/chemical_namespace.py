a559ffb1f337204dc610769d3d2a7538272d80aa
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-19 08:03:31 +0100

    Use constants module

--- src/bio2bel_chembl/chemical_namespace.py
+++ src/bio2bel_chembl/chemical_namespace.py
@@ -5,26 +5,22 @@ Download file ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_23
 Unzip chembl_23.db file into $HOME/.pybel/data/bio2bel/chembl/chembl_23_sqlite/
 """
 
+import click
 import logging
 import os
+import pandas as pd
 import sqlite3
 import sys
 
-import click
-import pandas as pd
-
-from pybel.constants import NAMESPACE_DOMAIN_CHEMICAL, PYBEL_DATA_DIR
+from pybel.constants import NAMESPACE_DOMAIN_CHEMICAL
 from pybel.resources.arty import get_today_arty_namespace
 from pybel.resources.definitions import write_namespace
 from pybel.resources.deploy import deploy_namespace
+from .constants import DATA_DIR
 
 log = logging.getLogger(__name__)
 
-CHEMBL_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'bio2bel', 'chembl')
-if not os.path.exists(CHEMBL_DATA_DIR):
-    os.makedirs(CHEMBL_DATA_DIR)
-
-CHEMBL_SQLITE3_DB_DIR = os.path.join(CHEMBL_DATA_DIR, 'chembl_23_sqlite')
+CHEMBL_SQLITE3_DB_DIR = os.path.join(DATA_DIR, 'chembl_23_sqlite')
 CHEMBL_DB = os.path.join(CHEMBL_SQLITE3_DB_DIR, "chembl_23.db")
 
 SQLITE_SELECT_TEXT = """
