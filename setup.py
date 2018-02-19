75650e468aee9b4968c6252a84f7587aa864e5e7
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-19 20:56:50 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ PACKAGES = setuptools.find_packages(where='src')
 META_PATH = os.path.join('src', 'bio2bel_chembl', '__init__.py')
 INSTALL_REQUIRES = [
     'click',
-    'pybel>=0.9.5',
-    'pybel_tools>=0.4.5',
-    'ols_client',
+    'pandas'
+    'pybel>=0.11.1',
+    'pybel_tools>=0.5.0',
+    'chembl_webresource_client',
 ]
 ENTRY_POINTS = {
     'bio2bel': [
